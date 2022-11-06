8a54342fefff387696c5e55aed90c3bd46a84d35
flashmike <mike@flashmike.com>
2022-11-06 15:26:23 +0400

    Latest update

--- src/App.js
+++ src/App.js
@@ -12,17 +12,17 @@ import Contact from "./pages/Contact";
 function App() {
   return (
     <div className="App">
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/menu" element={<Menu />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
-    
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+
     </div>
   );
 }
